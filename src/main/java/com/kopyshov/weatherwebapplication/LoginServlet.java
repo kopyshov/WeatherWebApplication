8dc07294fce5ec46a77601d35feac0bbaca5bcda
Dmitry Kopyshov <dkopysh@gmail.com>
2023-09-03 00:06:47 +0300

    Create HashGenerator.java for Token

--- src/main/java/com/kopyshov/weatherwebapplication/LoginServlet.java
+++ src/main/java/com/kopyshov/weatherwebapplication/LoginServlet.java
@@ -1,13 +1,16 @@
 package com.kopyshov.weatherwebapplication;
 
+import com.kopyshov.weatherwebapplication.auth.utils.HashGenerator;
 import com.kopyshov.weatherwebapplication.common.BasicServlet;
 import com.kopyshov.weatherwebapplication.dao.UsersDAO;
+import com.kopyshov.weatherwebapplication.entities.UserAuth;
 import com.kopyshov.weatherwebapplication.entities.UserData;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.io.IOException;
 
@@ -34,21 +37,36 @@ public class LoginServlet extends BasicServlet {
             session.setAttribute("loggedUser", user);
 
             if (rememberMe) {
-                //создаем токен (слектор, валидатор)
+                //создаем токен (селектор, валидатор)
+                generateToken(user);
                 //сохраняем токен
                 // создаем куки для селектора
                 //создаем куки для валидатора
             }
-
-            //Предыдущий код к которому вернуться если что
-//            currentSessions.put(user.getUsername(), user);
-//            Cookie cookie = new Cookie("username", user.getUsername());
-//            response.addCookie(cookie);
             response.sendRedirect(request.getContextPath() + "/home");
         } else {
             context.setVariable("error", "Unknown login, try again");
             templateEngine.process("login", context, response.getWriter());
         }
+    }
+
+    private static void generateToken(UserData user) {
+        UserAuth token = new UserAuth();
+
+        String selector = RandomStringUtils.randomAlphanumeric(12);
+        String rawValidator =  RandomStringUtils.randomAlphanumeric(64);
+
+        String hashedValidator = "";
+        try {
+            hashedValidator = HashGenerator.generateSHA256(rawValidator);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        token.setSelector(selector);
+        token.setValidator(hashedValidator);
+
+        token.setUser(user);
     }
 
     public void destroy() {
