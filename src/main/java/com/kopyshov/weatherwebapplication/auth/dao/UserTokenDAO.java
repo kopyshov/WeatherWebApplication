b4cb1a9a4c78374105e83f97e59dcd444d356b7e
Dmitry Kopyshov <dkopysh@gmail.com>
2023-09-03 01:28:23 +0300

    Update LoginFilter.java

--- src/main/java/com/kopyshov/weatherwebapplication/auth/dao/UserTokenDAO.java
+++ src/main/java/com/kopyshov/weatherwebapplication/auth/dao/UserTokenDAO.java
@@ -1,8 +1,12 @@
 package com.kopyshov.weatherwebapplication.auth.dao;
 
+import com.kopyshov.weatherwebapplication.auth.entities.UserData;
 import com.kopyshov.weatherwebapplication.auth.entities.UserToken;
 import com.kopyshov.weatherwebapplication.utils.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public enum UserTokenDAO {
     INSTANCE;
@@ -10,6 +14,29 @@ public enum UserTokenDAO {
         try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
             session.getTransaction().begin();
             session.persist(token);
+            session.flush();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public UserToken findBySelector(String selector) {
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Query<UserToken> query = session.createQuery("from UserToken where selector = :selector", UserToken.class);
+            query.setParameter("selector", selector);
+            List<UserToken> tokens = query.getResultList();
+            return tokens.get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void update(UserToken token) {
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            session.getTransaction().begin();
+            session.merge(token);
             session.flush();
             session.getTransaction().commit();
         } catch (Exception e) {
