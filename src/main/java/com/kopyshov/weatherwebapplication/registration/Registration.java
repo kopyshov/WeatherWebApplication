cd4df3db9644345fdd0a8590113e80fb7b3b5ef4
Dmitry Kopyshov <dkopysh@gmail.com>
2023-09-03 12:42:10 +0300

    Registration.java created

---
+++ src/main/java/com/kopyshov/weatherwebapplication/registration/Registration.java
@@ -0,0 +1,20 @@
+package com.kopyshov.weatherwebapplication.registration;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet("/registration")
+public class Registration extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
